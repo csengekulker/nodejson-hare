c72976bcd220f15ca521a7d6283c060f6f2dfe29
csengekulker <k71608883429@ktch.hu>
2022-09-22 17:07:13 +0200

    test+created dev env on pc

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 // get input: triangle base, height, calc area
-/*
+
+/* 
 store prev. calculations in database.json
 write this in TS please
 
 */
+
+
 console.log("mukodok varialok");
 console.log("anotha one");
+
+
